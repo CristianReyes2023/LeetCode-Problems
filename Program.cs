678dccc36445af304427904f1efbb4ac576f8bb3
“CristianReyes2023” <cristianlrm2019@gmail.com>
2023-12-06 20:31:06 -0500

    feat: :sparkles: LeetCode Problems

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //PALINDROMOOOO 
+
+            //PRIMERA FORMA
+            // Console.WriteLine("Inserte numero: ");
+            // int x = Convert.ToInt32(Console.ReadLine());
+            // if (x < 0)
+            // {
+            //     Console.WriteLine("false");
+            // }
+            // string palidromo = Convert.ToString(x);
+            // string confirpali = "";
+            // for (int j = palidromo.Length - 1; j >= 0; j--)
+            // {
+            //     confirpali = confirpali + palidromo[j];
+            // }
+            // if(confirpali == palidromo)
+            // {
+            //     Console.WriteLine("Son palindromo");
+            // }
+            // else{
+            //     Console.WriteLine("no son palindromo");
+            // }
+
+            //SEGUNDA FORMA
+            // int x = 34543;
+            // int r = 0, c = x;
+            // while (c > 0)
+            // {
+            //     r = r * 10 + c % 10;
+            //     c /= 10;
+            // }
+            // Console.WriteLine(x);
+            // return r == x;
+
+
+        //ROMAN INTEGER
+    }
+}
